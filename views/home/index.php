b6c11e228d04774bce2f086f0173ea12b8fbae00
Doron Davidowitz <dorondavidowitz@gmail.com>
2018-07-02 15:57:51 +0300

    test

--- views/home/index.php
+++ views/home/index.php
@@ -2,29 +2,71 @@
     header('Location: ' . ROOT_URL . 'users/login');
 endif;?>
 <style>
-    .coursesSideNav {
-        background: pink;
-        text-align: center;
-        grid-column: 3/5;
-        grid-row: 1;
+    .mainNavBar {
+        border: 2px solid #C92240;
+        border-radius: 25px;
         padding: 15px;
+        grid-column: 1/4;
+        display: grid;
 
+    }
+
+    .mainNavBar ul {
+        display: grid;
+        grid-auto-rows: 80px;
+        list-style-type: none;
+        grid-template-columns: repeat(4, 1fr);
+        width: 100%;
+        grid-gap: 5px;
 
 
     }
 
-    .studentSideNav {
-        background: hotpink;
-        text-align: center;
+    #studentSideHeadId {
         grid-column: 1/3;
-        grid-row: 1;
-        padding: 15px;
+        /* background: greenyellow; */
+    }
 
+    #coursesSideHeadId {
+        grid-column: 3/6;
+        /* background: teal; */
+    }
+
+    .studentSideBody {
+        grid-column: 1/3;
+        /* background: fuchsia; */
+        border: 2px solid #C92240;
+
+    }
+
+    .coursesSideBody {
+        grid-column: 3/6;
+        /* background: darkorange; */
+        border: 2px solid #C92240;
+    }
+
+    .lestSideHeader {
+        text-align: center;
+        padding: 25px;
+        border: 2px solid #C92240;
+        text-decoration: underline;
 
     }
 </style>
 
 <div class="mainNavBar">
-    <div class="coursesSideNav">Courses</div>
-    <div class="studentSideNav">Student</div>
+
+    <ul>
+        <li class="lestSideHeader" id="studentSideHeadId">Student</li>
+        <li class="lestSideHeader" id="coursesSideHeadId">Courses</li>
+        <li class="studentSideBody">Student</li>
+        <li class="coursesSideBody">Courses</li>
+        <li class="studentSideBody">Student</li>
+        <li class="coursesSideBody">Courses</li>
+        <li class="studentSideBody">Student</li>
+        <li class="coursesSideBody">Courses</li>
+    </ul>
+</div>
+<div class="mainScreen">
+
 </div>
